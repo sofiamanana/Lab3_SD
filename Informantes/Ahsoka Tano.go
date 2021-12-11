5172daa4610a3996ef856d1ecd3ae3382fbec615
Sofia Mañana <sofiamanana@sansano.usm.cl>
2021-12-10 23:31:35 -0300

    blabla

--- Informantes/Ahsoka Tano.go
+++ Informantes/Ahsoka Tano.go
@@ -31,24 +31,27 @@ func main(){
 	log.Printf("[4] Borrar una ciudad.\n")
 	log.Printf("[5] No quiero hacer ni una wea más.\n")
 	var opcion int32
-	fmt.Scan(&opcion)
-	if (opcion==1){
-		response, err := c.QuieroHacer(context.Background(), &pb.Comando{Comando: "AddCity"})
-		if err != nil {
-		log.Fatalf("Error when calling QuieroHacer: %s", err)
+	while (opcion != 5){
+		fmt.Scan(&opcion)
+		if (opcion==1){
+			response, err := c.QuieroHacer(context.Background(), &pb.Comando{Comando: "AddCity"})
+			if err != nil {
+			log.Fatalf("Error when calling QuieroHacer: %s", err)
+			}
+			log.Printf("Respuesta del Broker: %d", response.Valor)
+			log.Printf("¿Cuál es el nombre del planeta?:\n")
+			var planeta string
+			fmt.Scan(&planeta)
+			log.Printf("¿Cuál es el nombre de la ciudad?:\n")
+			var ciudad string
+			fmt.Scan(&ciudad)
+			log.Printf("¿Cuántos rebeldes?:\n")
+			var rebeldes int32
+			fmt.Scan(&rebeldes)
+			//aquí debería enviar los datos al fulcrum de response.valor 
 		}
-		log.Printf("Respuesta del Broker: %d", response.Valor)
-		log.Printf("¿Cuál es el nombre del planeta?:\n")
-		var planeta string
-		fmt.Scan(&planeta)
-		log.Printf("¿Cuál es el nombre de la ciudad?:\n")
-		var ciudad string
-		fmt.Scan(&ciudad)
-		log.Printf("¿Cuántos rebeldes?:\n")
-		var rebeldes int32
-		fmt.Scan(&rebeldes)
-		
 	}
+	
 	
 }
 
